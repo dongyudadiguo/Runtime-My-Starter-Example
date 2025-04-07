b0da9a119673ca0890878b22dbba1934ef6bee16
dongyudadiguo <3662728251@qq.com>
2025-04-07 16:31:33 +0800

    Update run.c

--- run.c
+++ run.c
@@ -23,54 +23,75 @@ void test(void){
 }
 
 void (*fun[])(void) = { test };
-size_t fun_size = sizeof(fun) / sizeof(fun[0]);
+size_t fun_size = sizeof(fun) / sizeof(fun[0]);  // 使用size_t类型
 char *str = "test\0";
 
 
 
 void load(void){
     
+    static char searchText[256] = {0};
     static int scrollIndex = 0;
+
     static int selected_start = -1;
     static int selected_end = -1;
     static int fixed_start = 0;
     static int fixed_size = 0;
     static int relative = 0;
-
+    
     int wheel = GetMouseWheelMove();
     scrollIndex -= wheel;
     if (scrollIndex < 0) scrollIndex = 0;
 
-    char* current = str;
-    int totalStrings = 0;
-    while (*current != '\0' && totalStrings < 256) {
-        current += strlen(current) + 1;
-        totalStrings++;
+    int key = GetCharPressed();
+    while (key > 0) {
+        if (strlen(searchText) < 255) {
+            searchText[strlen(searchText)] = (char)key;
+            scrollIndex = 0;
+        }
+        key = GetCharPressed();
+    }
+    if (IsKeyPressed(KEY_BACKSPACE) && strlen(searchText) > 0) {
+        searchText[strlen(searchText)-1] = '\0';
+        scrollIndex = 0;
     }
 
-    if (scrollIndex > totalStrings - 1) scrollIndex = totalStrings > 0 ? totalStrings - 1 : 0;
+    char* current = str;
+    int originalTotal = 0;
+    int filteredTotal = 0;
+    while (*current != '\0' && originalTotal < 11011) {
+        if (strstr(current, searchText)) filteredTotal++;
+        current += strlen(current) + 1;
+        originalTotal++;
+    }
+    
+    if (scrollIndex > filteredTotal - 1) scrollIndex = filteredTotal > 0 ? filteredTotal - 1 : 0;
 
     current = str;
     int currentIndex = 0;
     int yPos = 30;
-    while (*current != '\0' && currentIndex < totalStrings) {
-        if (currentIndex >= scrollIndex) {
-            Rectangle textRect = { 10, (float)yPos, (float)MeasureText(current, 10), 20 };
-            DrawText(current, 10, yPos, 10, WHITE);
-            if (CheckCollisionPointRec(GetMousePosition(), textRect)) {
-                if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
-                    fun[currentIndex]();
+    int visibleIndex = 0;
+    while (*current != '\0' && currentIndex < originalTotal) {
+        if (strstr(current, searchText)) {
+            if (visibleIndex >= scrollIndex) {
+                Rectangle textRect = { 10, (float)yPos, (float)MeasureText(current, 10), 20 };
+                DrawText(current, 10, yPos, 10, WHITE);
+                if (CheckCollisionPointRec(GetMousePosition(), textRect)) {
+                    if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON)) {
+                        fun[currentIndex]();
+                    }
+                    DrawRectangleLinesEx(textRect, 1, YELLOW);
                 }
-                DrawRectangleLinesEx(textRect, 1, YELLOW);
+                yPos += 20;
             }
-            yPos += 20;
+            visibleIndex++;
         }
         current += strlen(current) + 1;
         currentIndex++;
     }
 
-    int max_chars = strlen((char*)std);
-    if (max_chars > 25) max_chars = 25;
+
+    int max_chars = 25;
     
     int currentX = 10;
     for (int i = 0; i < max_chars; i++) {
@@ -130,7 +151,7 @@ int main(){
     
     InitWindow(800, 600, "run.c");
     std = malloc(256);
-    strcpy((char*)std, "hello world\0");
+    strcpy((char*)std, "1234567890\0");
     
     while (1) imp();
 }
