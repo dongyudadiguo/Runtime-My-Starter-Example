6cf21de597d5d1cc908e36393a053cda688e5467
dongyudadiguo <3662728251@qq.com>
2025-04-07 14:37:04 +0800

    Update run.c

--- run.c
+++ run.c
@@ -27,14 +27,14 @@ size_t fun_size = sizeof(fun) / sizeof(fun[0]);  // 使用size_t类型
 char *str = "test\0";
 
 void load(void){
-
+    
     static int scrollIndex = 0;
-    static int selected_start = 0;
-    static int selected_end = 0;
+    static int selected_start = -1;
+    static int selected_end = -1;
     static int fixed_start = 0;
     static int fixed_size = 0;
     static int relative = 0;
-    
+
     int wheel = GetMouseWheelMove();
     scrollIndex -= wheel;
     if (scrollIndex < 0) scrollIndex = 0;
@@ -67,44 +67,52 @@ void load(void){
         currentIndex++;
     }
 
-    for (int i = 0; i < 25; i++) {
+    int max_chars = strlen((char*)std);
+    if (max_chars > 25) max_chars = 25;
+    
+    int currentX = 10;
+    for (int i = 0; i < max_chars; i++) {
         char currentChar = ((char*)std)[i];
-        int textWidth = MeasureText(TextFormat("%c", currentChar), 10);
-        int xPos = 10 + textWidth * i;
+        int textWidth = MeasureText(TextFormat("%c", currentChar), 20);  // 字体大小20
         
-        Color textColor = (i >= selected_start && i <= selected_end) ? WHITE : GRAY;
-        DrawText(TextFormat("%c", currentChar), xPos, 10, 10, textColor);
+        int xPos = currentX;
+        currentX += textWidth;
 
-        int mouseX = GetMouseX();
-        int mouseY = GetMouseY();
-        bool inXRange = (mouseX > xPos) && (mouseX < xPos + textWidth);
-        bool inYRange = (mouseY > 10) && (mouseY < 20);
+        Color textColor = (i >= selected_start && i <= selected_end && selected_start >= 0) ? WHITE : GRAY;
+        DrawText(TextFormat("%c", currentChar), xPos, 10, 20, textColor);  // 字体大小20
 
-        if (inXRange && inYRange) {
+        Rectangle charRect = { (float)xPos, 10, (float)textWidth, 20 };
+        if (CheckCollisionPointRec(GetMousePosition(), charRect)) {
             if (IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
-                selected_start = i;
-            }
-            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
-                fixed_start = selected_start;
-                fixed_size = selected_end - selected_start;
-                relative = i - fixed_start;
+                selected_start = selected_end = i;
             }
             if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
                 selected_end = i;
             }
+            if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
+                fixed_start = selected_start;
+                fixed_size = selected_end - selected_start;
+                relative = i - selected_start;
+            }
             if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
-                selected_start = i - relative;
-                selected_end = selected_start + fixed_size;
+                int newStart = i - relative;
+                int newEnd = newStart + fixed_size;
+                if (newEnd < max_chars && newStart >= 0) {
+                    selected_start = newStart;
+                    selected_end = newEnd;
+                }
             }
             if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT)) {
-                memmove((char*)std + fixed_start, (char*)std + selected_start, 
-                       selected_end - selected_start);
-                selected_start = selected_end = fixed_start = fixed_size = 0;
+                if (selected_start >= 0 && selected_end < max_chars && 
+                    fixed_start >= 0 && (fixed_start + fixed_size) < max_chars) {
+                    memmove((char*)std + fixed_start, 
+                           (char*)std + selected_start, 
+                           selected_end - selected_start + 1);
+                }
+                selected_start = selected_end = fixed_start = fixed_size = -1;
             }
         }
     }
-    
-
 
     EndDrawing();
     ClearBackground(BLACK);
@@ -120,6 +128,7 @@ int main(){
     
     InitWindow(800, 600, "run.c");
     std = malloc(256);
+    strcpy((char*)std, "hello world\0");
     
     while (1) imp();
 }
