8f7d1b2e6c723df84082214f796e945da8dbc675
dongyudadiguo <3662728251@qq.com>
2025-04-23 18:29:37 +0800

    Update and rename Runtime-Starter-Example.c to Runtime-Operator-Example.c

--- Runtime-Operator-Example.c
+++ Runtime-Operator-Example.c
@@ -2,13 +2,23 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "Runtime-lib-raylib-statement.h"
+
 void starter(void);
+
+int stack[1024];
+int stackPtr = 0;
 
 void *buffer;
 int ptr;
-void *std;
-void (*fun[])(void) = {starter};
+void (*fun[])(void) = {starter, InitWindow__, CloseWindow__};
 void (*imp)(); 
+
+void exit__(void){
+    imp = fun[*(int*)(buffer + (ptr = stack[--stackPtr] + sizeof(int)))];
+}
+
+#include "Runtime-lib-raylib.h"
 
 void starter(void)
 {
@@ -17,43 +27,41 @@ void starter(void)
     static int fixed_start = 0;
     static int fixed_size = 0;
     static int relative = 0;
+    static int type_location = 0;
     static bool isSelecting = true;
-    static bool isInputting = true;
-    //static char inputText[256] = "";
 
-    int key = GetCharPressed();
-    while (key > 0)
-    {
-        char *tmp;
-        (tmp = (char*)(std + strlen(std)))[0] = (char)key;
-        tmp[1] = '\0';
-        key = GetCharPressed();
-        isInputting = true;
-    }
-    if (IsKeyPressed(KEY_ENTER)){
-        if (isInputting == 0)
+    char *tmp = buffer + type_location;
+    if (IsKeyPressed(KEY_BACKSPACE)) {
+        *(tmp) = '\0';
+    }else{
+        int key = GetCharPressed();
+        while (key > 0)
         {
-            fun[*(int*)std];
+            *(tmp = (char*)(tmp + strlen(tmp))) = (char)key;
+            *(tmp + 1) = '\0';
+            key = GetCharPressed();
         }
-        *(int*)std = atoi(std);
-        ((char*)std)[sizeof(int)] = '\0';
-        isInputting = false;
     }
-
+    
     int currentX = 10;
     for (int i = 0; i < 256; i++)
     {
-        char currentChar = ((char *)std)[i];
+        char currentChar = ((char *)buffer)[i];
         int textWidth = MeasureText(TextFormat("%c", currentChar), 20) + 2; // 字体大小20, 额外的2像素
 
         int xPos = currentX;
         currentX += textWidth;
 
+        if (i == type_location) DrawRectangle(xPos, 10, textWidth, 20, GREEN);
+        
         bool isSelected = (i >= selected_start && i <= selected_end && selected_start >= 0);
         DrawText(TextFormat("%c", currentChar), xPos, 10, 20, isSelected ? WHITE : GRAY); // 字体大小20
 
         Rectangle charRect = {xPos, 10, textWidth, 20};
         if (CheckCollisionPointRec(GetMousePosition(), charRect)) {
+            if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+                type_location = i;
+            }
             if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT)) {
                 if (isSelecting) {
                     selected_start = selected_end = i;
@@ -75,8 +83,8 @@ void starter(void)
                 if (isSelecting) {
                     isSelecting = false;
                 } else {
-                    memmove((char *)std + selected_start,
-                            (char *)std + fixed_start,
+                    memmove((char *)buffer + selected_start,
+                            (char *)buffer + fixed_start,
                             fixed_size + 1);
                     selected_start = selected_end = fixed_start = fixed_size = -1;
                     isSelecting = true;
@@ -89,14 +97,11 @@ void starter(void)
     BeginDrawing();
     ClearBackground(BLACK);
 
-    imp = fun[(ptr = 0)];
+    *(int*)(buffer + (ptr = 0));
 }
 
 int main(){
     InitWindow(800, 450, "Runtime-Starter-Example");
-    imp = fun[ptr = (*(int*)(buffer = malloc(sizeof(void *))) = 0)];
-    std = malloc(256);
-    char* str = "\0Runtime-Starter-Example";
-    memcpy(std, str, strlen(str + 1) + 1);
+    imp = fun[ptr = (*(int*)(buffer = malloc(sizeof(void *)*256)) = 0)];
     while (1){ imp();}
 }
